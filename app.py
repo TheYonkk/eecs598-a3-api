c103eff7312f561c8f25b52eb62235a2a204dd98
Dave Yonkers <dyonkers@prattmiller.com>
2023-02-07 14:27:11 -0500

    update reqs

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def ask():
     return jsonify(ans)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, ssl_context='adhoc')
+    app.run(debug=True, host="0.0.0.0", port=5000)
